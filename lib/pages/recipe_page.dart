bb6266ee29c0068f5fa98560630edd07c3916267
md-siam <md.siam03@gmail.com>
2023-08-26 19:45:27 +0600

    feat: internet connection checker removed

--- lib/pages/recipe_page.dart
+++ lib/pages/recipe_page.dart
@@ -1,16 +1,11 @@
-import 'dart:developer';
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:recipe_app/pages/recipe_detail_page.dart';
 
 import '../core/app_color.dart';
 import '../view_models/recipe_info_provider.dart';
-import '../widgets/empty_search.dart';
 import '../widgets/recipe_card.dart';
+import 'recipe_detail_page.dart';
 
 class RecipePage extends StatefulWidget {
   const RecipePage({Key? key}) : super(key: key);
@@ -22,32 +17,12 @@ class RecipePage extends StatefulWidget {
 class _RecipePageState extends State<RecipePage> {
   final TextEditingController _controller = TextEditingController();
   late final RecipeInfoProvider photoModel;
-  bool _isConnected = false;
 
   @override
   void initState() {
     super.initState();
-    _checkInternetConnection();
     photoModel = Provider.of<RecipeInfoProvider>(context, listen: false);
     photoModel.getPhotoData('duck');
-  }
-
-  Future<void> _checkInternetConnection() async {
-    try {
-      final response = await InternetAddress.lookup('www.google.com');
-      if (response.isNotEmpty) {
-        setState(() {
-          _isConnected = true;
-        });
-      }
-    } on SocketException catch (err) {
-      setState(() {
-        _isConnected = false;
-      });
-      if (kDebugMode) {
-        log(err.toString());
-      }
-    }
   }
 
   @override
@@ -59,66 +34,62 @@ class _RecipePageState extends State<RecipePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        iconTheme: const IconThemeData(color: AppColors.kDeepGrey),
-        backgroundColor: AppColors.kLightGrey,
-        leading: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Image.asset(
-            'asset/images/app_icon.png',
-          ),
-        ),
-        title: SearchBar(
-          padding: const MaterialStatePropertyAll(
-            EdgeInsetsDirectional.only(start: 10, end: 0.0),
-          ),
-          hintStyle: MaterialStatePropertyAll(
-            TextStyle(color: Colors.grey.shade600),
-          ),
-          leading: const Icon(Icons.search),
-          hintText: 'Search...',
-          controller: _controller,
-          elevation: const MaterialStatePropertyAll(0.0),
-          constraints: const BoxConstraints(
-            maxHeight: 40,
-          ),
-          trailing: [
-            IconButton(
-              icon: const Icon(Icons.clear),
-              onPressed: () {
-                _controller.clear();
-              },
-            )
-          ],
-          // onChanged: (newValue) {
-          //   print(newValue);
-          // },
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(
-              Icons.menu,
-              size: 30,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: AppColors.kDeepGrey),
+          backgroundColor: AppColors.kLightGrey,
+          leading: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Image.asset(
+              'asset/images/app_icon.png',
             ),
-            onPressed: () {},
           ),
-        ],
-      ),
-      body: _isConnected
-          ? Consumer<RecipeInfoProvider>(
-              builder: (context, provider, child) {
-                return photoModel.isLoading
-                    ? const Center(child: CircularProgressIndicator())
-                    : Padding(
-                        padding: const EdgeInsets.all(8)
-                            .copyWith(top: 15, bottom: 0),
-                        child: _customGripViewBuilder(provider),
-                      );
-              },
-            )
-          : const EmptySearch(),
-    );
+          title: SearchBar(
+            padding: const MaterialStatePropertyAll(
+              EdgeInsetsDirectional.only(start: 10, end: 0.0),
+            ),
+            hintStyle: MaterialStatePropertyAll(
+              TextStyle(color: Colors.grey.shade600),
+            ),
+            leading: const Icon(Icons.search),
+            hintText: 'Search...',
+            controller: _controller,
+            elevation: const MaterialStatePropertyAll(0.0),
+            constraints: const BoxConstraints(
+              maxHeight: 40,
+            ),
+            trailing: [
+              IconButton(
+                icon: const Icon(Icons.clear),
+                onPressed: () {
+                  _controller.clear();
+                },
+              )
+            ],
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.menu,
+                size: 30,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Consumer<RecipeInfoProvider>(
+          builder: (context, provider, child) {
+            return photoModel.isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : Padding(
+                    padding:
+                        const EdgeInsets.all(8).copyWith(top: 15, bottom: 0),
+                    child: _customGripViewBuilder(provider),
+                  );
+          },
+        )
+        // : const EmptySearch(),
+        );
   }
 
   /// displays all the recipe in [grid view]
